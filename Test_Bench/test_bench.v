1f4aaeb99b3f6337db116a6daae43df5a64c3c79
Mohamed Lotfy <molotfy657@gmail.com>
2025-11-29 21:50:26 +0200

    DSP

--- Test_Bench/test_bench.v
+++ Test_Bench/test_bench.v
@@ -20,7 +20,7 @@ reg [47:0] C_tb,PCIN_tb;
 reg [7:0] OPMODE_tb;
 reg CLK_tb,CARRYIN_tb,RSTA_tb,RSTB_tb,RSTC_tb,RSTD_tb,RSTM_tb,RSTCARRYIN_tb
 ,RSTOPMODE_tb,RSTP_tb,CEA_tb,CEB_tb,CEC_tb,CED_tb,CECARYIN_tb,CEM_tb,CEOPMODE_tb,CEP_tb;
-// DEFINE OUTPUTS OP INSTANTIATION
+// DEFINE INSTANTIATION optputs
 wire [17:0] BCOUT_dut;
 wire [47:0] PCOUT_dut,P_dut;
 wire [35:0] M_dut;
@@ -35,6 +35,7 @@ DSP48A1 DUT(A_tb,B_tb,C_tb,D_tb,CARRYIN_tb,M_dut,P_dut,CARRYOUT_dut,CARRYOUTF_du
 CEA_tb,CEB_tb,CEC_tb,CED_tb,CECARYIN_tb,CEM_tb,CEOPMODE_tb,CEP_tb,RSTA_tb,RSTB_tb,RSTC_tb,RSTD_tb
 ,RSTM_tb,RSTCARRYIN_tb,RSTOPMODE_tb,RSTP_tb,PCIN_tb,BCIN_tb,BCOUT_dut,PCOUT_dut);
 
+integer i;
 // CLOCK GENERATION
 initial begin
     CLK_tb =0;
@@ -54,15 +55,15 @@ initial begin
     RSTCARRYIN_tb = 1;
     RSTOPMODE_tb = 1;
     RSTP_tb = 1;
-    // DISABLE ALL CLOCK ENABLES
-    CEA_tb = 0;
-    CEB_tb = 0;
-    CEC_tb = 0;
-    CED_tb = 0;
-    CECARYIN_tb = 0;
-    CEM_tb = 0;
-    CEOPMODE_tb = 0;
-    CEP_tb = 0;
+    // ENABLE ALL NECESSARY CLOCK ENABLES
+    CEA_tb = 1;
+    CEB_tb = 1;
+    CEC_tb = 1;
+    CED_tb = 1;
+    CECARYIN_tb = 1;
+    CEM_tb = 1;
+    CEOPMODE_tb = 1;
+    CEP_tb = 1;
     // INITIALIZE ALL INPUT SIGNALS TO ZERO
     A_tb = 0;
     B_tb = 0;
@@ -80,7 +81,7 @@ initial begin
     BCOUT_expected = 0;
     PCOUT_expected = 0;
 
-    repeat(1) @(negedge CLK_tb);
+    @(negedge CLK_tb);
 
     // RELEASE ALL RESET SIGNALS
     RSTA_tb = 0;
@@ -91,38 +92,146 @@ initial begin
     RSTCARRYIN_tb = 0;
     RSTOPMODE_tb = 0;
     RSTP_tb = 0;
-    // ENABLE ALL NECESSARY CLOCK ENABLES
-    CEA_tb = 1;
-    CEB_tb = 1;
-    CEC_tb = 1;
-    CED_tb = 1;
-    CECARYIN_tb = 1;
-    CEM_tb = 1;
-    CEOPMODE_tb = 1;
-    CEP_tb = 1;
-    repeat(10) begin
-        OPMODE_tb = $random;
-        A_tb = $random % 16;
-        B_tb = $random % 16;
-        C_tb = $random % 16;
-        D_tb = $random % 16;
-        P_expected = ((D_tb + B_tb) * A_tb) + C_tb;
-        PCOUT_expected = P_expected;
-        M_expected = (D_tb + B_tb) * A_tb;
-        BCOUT_expected = D_tb + B_tb;
-        CARRYOUT_expected = 0;
-        CARRYOUTF_expected = 0;
-        repeat(4) @(negedge CLK_tb);
-        if ((P_expected != P_dut) || (PCOUT_expected != PCOUT_dut) || (M_expected != M_dut) ||
-            (BCOUT_expected != BCOUT_dut) || (CARRYOUT_expected != CARRYOUT_dut) || 
-            (CARRYOUTF_expected != CARRYOUTF_dut)) begin
-            $display("Error");
-            $stop;
+    for(i=0;i<99;i=i+1)begin
+        OPMODE_tb =$random;
+        A_tb = $urandom_range(1,500);
+        B_tb = $urandom_range(1,500);
+        C_tb = $urandom_range(1,500);
+        D_tb = $urandom_range(1,500);
+        PCIN_tb = $urandom_range(1,500);
+        BCIN_tb = $urandom_range(1,500);
+        CARRYIN_tb = $random;
+         repeat(3) @(negedge CLK_tb);
+        // TEST BOUT OUTPUT
+        if(OPMODE_tb[4])begin
+            if(OPMODE_tb[6])
+              BCOUT_expected = D_tb - B_tb;
+            else
+              BCOUT_expected =  D_tb + B_tb;
+        end
+        else begin
+          BCOUT_expected = B_tb;
+        end
+        // TEST MULTIPLICATION OUTPUT
+         M_expected = BCOUT_expected * A_tb;
+        #20;
+        if((M_expected != M_dut) || (BCOUT_expected != BCOUT_dut))begin
+          $display("Error");
+          $stop;
         end
     end
+    //  TEST P AND CARRYOUT AT OPMODE 0000_0010
+    OPMODE_tb = 2;
+    for(i=0;i<49;i=i+1)begin
+        A_tb = $urandom_range(1,500);
+        B_tb = $urandom_range(1,500);
+        C_tb = $urandom_range(1,500);
+        D_tb = $urandom_range(1,500);
+        PCIN_tb = $urandom_range(1,500);
+        BCIN_tb = $urandom_range(1,500);
+         {CARRYOUT_expected,P_expected} = P_expected;
+            repeat(4) @(negedge CLK_tb);
+            if((P_expected != P_dut) && (CARRYOUT_expected != CARRYOUT_dut))begin
+              $display("Error");
+              $stop;
+            end
+    end
+    //  TEST P AND CARRYOUT WHEN OUTPUT OF X_MUX IS {D_tb[11:0],A_tb,B_tb} AND Z_MUX IS ZERO
+    OPMODE_tb = 3;
+    for(i=0;i<49;i=i+1)begin
+        A_tb = $urandom_range(1,500);
+        B_tb = $urandom_range(1,500);
+        C_tb = $urandom_range(1,500);
+        D_tb = $urandom_range(1,500);
+        PCIN_tb = $urandom_range(1,500);
+        BCIN_tb = $urandom_range(1,500);
+             {CARRYOUT_expected,P_expected} = {D_tb[11:0],A_tb,B_tb};
+              repeat(4) @(negedge CLK_tb);
+              if((P_expected != P_dut) && (CARRYOUT_expected != CARRYOUT_dut))begin
+                $display("Error");
+                $stop;
+              end
+    end
+    //  TEST P AND CARRYOUT WHEN OUTPUT OF X_MUX IS ZERO AND Z_MUX IS ZERO
+    OPMODE_tb = 0;
+    for(i=0;i<49;i=i+1)begin
+        A_tb = $urandom_range(1,500);
+        B_tb = $urandom_range(1,500);
+        C_tb = $urandom_range(1,500);
+        D_tb = $urandom_range(1,500);
+        PCIN_tb = $urandom_range(1,500);
+        BCIN_tb = $urandom_range(1,500);
+
+             {CARRYOUT_expected,P_expected} = 0;
+
+              repeat(4) @(negedge CLK_tb);
+              if((P_expected != P_dut) && (CARRYOUT_expected != CARRYOUT_dut))begin
+                $display("Error");
+                $stop;
+              end
+    end
+    //  TEST P AND CARRYOUT WHEN OUTPUT OF X_MUX IS MULTIPLICATION AND Z_MUX IS ZERO
+     OPMODE_tb = 1;
+    for(i=0;i<49;i=i+1)begin
+        A_tb = $urandom_range(1,500);
+        B_tb = $urandom_range(1,500);
+        C_tb = $urandom_range(1,500);
+        D_tb = $urandom_range(1,500);
+        PCIN_tb = $urandom_range(1,500);
+        BCIN_tb = $urandom_range(1,500);
+        if(OPMODE_tb[4])begin
+            if(OPMODE_tb[6])
+              BCOUT_expected = D_tb - B_tb;
+            else
+              BCOUT_expected =  D_tb + B_tb;
+        end
+        else begin
+          BCOUT_expected = B_tb;
+        end
+         M_expected = BCOUT_expected * A_tb;
+        {CARRYOUT_expected,P_expected} = M_expected;
+            repeat(4) @(negedge CLK_tb);
+            if((P_expected != P_dut) && (CARRYOUT_expected != CARRYOUT_dut))begin
+              $display("Error");
+              $stop;
+            end
+    end
+    //  TEST P AND CARRYOUT WHEN OUTPUT OF X_MUX IS ZERO AND Z_MUX IS PCIN
+    OPMODE_tb = 8'b0000_0100;
+    for(i=0;i<49;i=i+1)begin
+        A_tb = $urandom_range(1,500);
+        B_tb = $urandom_range(1,500);
+        C_tb = $urandom_range(1,500);
+        D_tb = $urandom_range(1,500);
+        PCIN_tb = $urandom_range(1,500);
+        BCIN_tb = $urandom_range(1,500);
+        {CARRYOUT_expected,P_expected} = PCIN_tb;
+            repeat(4) @(negedge CLK_tb);
+            if((P_expected != P_dut) && (CARRYOUT_expected != CARRYOUT_dut))begin
+              $display("error");
+              $stop;
+            end
+    end
+    //  TEST P AND CARRYOUT WHEN OUTPUT OF X_MUX IS ZERO AND Z_MUX IS C
+    OPMODE_tb = 8'b0000_1100;
+    for(i=0;i<49;i=i+1)begin
+        A_tb = $urandom_range(1,500);
+        B_tb = $urandom_range(1,500);
+        C_tb = $urandom_range(1,500);
+        D_tb = $urandom_range(1,500);
+        PCIN_tb = $urandom_range(1,500);
+        BCIN_tb = $urandom_range(1,500);
+        {CARRYOUT_expected,P_expected} = C_tb;
+            repeat(4) @(negedge CLK_tb);
+            if((P_expected != P_dut) && (CARRYOUT_expected != CARRYOUT_dut))begin
+              $display("error");
+              $stop;
+            end
+    end
+
      $stop;
 end
 initial begin
-    $monitor("A=%d,B=%d,C=%d,D=%d,M=%d,P=%d",A_tb,B_tb,C_tb,D_tb,M_expected,P_expected);
+    $monitor("A=%d,B=%d,C=%d,D=%d,BCOUT=%d,M=%d,P=%d,P_dut=%d",A_tb,B_tb,C_tb,D_tb,BCOUT_expected,M_expected,P_expected,P_dut);
 end
 endmodule
